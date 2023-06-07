594d59515ce65a2a6715b02ac439795338c0eada
park4608 <43196104+park4608@users.noreply.github.com>
2023-06-07 15:38:03 +0900

    new imgs & img directory arrangement

---
+++ src/components/boardComponent/boardTile/TileImgSrc.ts
@@ -0,0 +1,17 @@
+import { ImgSrcProps } from '../../../interface/interfaces';
+
+export const ImgSrc: ImgSrcProps = {
+  '양 시장': 'animal/sheep',
+  울타리: 'tile/fenceMaking',
+  '곡식 활용': 'tile/grain_bread',
+  '급하지 않은 가족 늘리기': 'tile/slowFamilyMaking_t',
+  '서부 채석장': 'resource/stone',
+  '집 개조': 'tile/houseRemodeling',
+  '돼지 시장': 'animal/wildboar',
+  '채소 종자': 'resource/vegetable',
+  '소 시장': 'animal/cow',
+  '동부 채석장': 'resource/stone',
+  밭농사: '',
+  '급한 가족 늘리기': 'tile/fastFamilyMaking_t',
+  '농장 개조': 'tile/farmRemodeling',
+};
